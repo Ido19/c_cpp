d8c2bd93a0d3efef2680636ddac05a5d6435d2d4
H3R0K1NX <eld4n.g@gmail.com>
2019-12-21 21:51:12 +0200

    Add solution to ex2

---
+++ Mahat/ex2.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+int *func(char *str);
+
+int *func(char *str){
+    int i = 0;
+    int count = 0;
+    int flag  = 0;
+    for(i ; i < strlen(str); i++){
+        if((int)str[i] < 'A' || (int)str[i] > 'z'){
+            if((int)str[i] != ' '){
+            int *x = malloc(sizeof(int));
+            *x = -1;
+            return x;
+            }
+            else{
+                count++;
+            }
+        }
+    }//end of for
+    int* arr;
+    arr = (int *)malloc(sizeof(int)*(count+1));
+
+    for(i=0; i < sizeof(arr); i++){
+        arr[i] = 0;
+    }
+
+    int j = 0;
+    int z = 0;
+    i = 0;
+    for(i ; i < strlen(str); i++){
+        // if((int)str[i] != ' ' && i != strlen(str)-1)
+        //     j++;
+        // else// else if((int)str[i] == ' ' || i == strlen(str)-1)
+        // {
+        //     arr[z] = j;
+        //     z++;
+        //     j=0;
+        // }
+        if(i == strlen(str)-1){
+            j++;
+            arr[z] = j;
+        }
+        else if((int)str[i] == ' '){
+            arr[z] = j;
+            z++;
+            j=0;
+        }
+        else{
+            j++;
+        }
+    }
+    arr[count+1] = -1;
+
+    for (i = 0; i < sizeof(arr); i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    
+
+    return arr;
+}
+
+
+int main(){
+    int *x;
+    x = func("dog cat ronenen");
+    printf("Value of x= %d", *x);
+    return 0;
+}
